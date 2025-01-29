ac6f8763814d2f8d8d1e9825eadc6177cf3a87f3
DanikaMarieSPanares <danikamarie.panares@smc.pshs.edu.ph>
2025-01-29 23:21:38 +0800

    Delete item2.js

--- item2.js
+++
@@ -1,8 +0,0 @@
-let firstName = prompt("Enter your first name:");
-let lastName = prompt("Enter your last name:");
-let birthYear = prompt("Enter your birth year:")
-
-var currentYear = new Date().getFullYear();
-var age = currentYear - parseInt(birthYear);
-
-document.getElementById("greet").innerHTML = `Hello ${firstName} ${lastName}! How does it feel to be ${age} years old?`;
